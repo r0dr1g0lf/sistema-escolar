caccdeb0d7df4e495a044e76448cbbca069befbe
r0dr1g0lf <rpdrigolf@gmail.com>
2026-04-26 21:57:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -240,13 +240,23 @@ else:
                 
                 df_exibicao = df_filtrado.rename(columns=mapeamento_colunas)
                 
-                # Ordenação solicitada: Bimestre (Periodo), Turma e depois Aluno (ordem alfabética)
                 df_exibicao = df_exibicao.sort_values(by=["Periodo", "Turma", "Aluno"])
                 
                 ordem_colunas = ["Turma", "Aluno", "Periodo", "Disciplina", "Professor", "Tipo_Registro", "Descrição_Detalhada"]
                 df_exibicao = df_exibicao[ordem_colunas]
                 
-                st.dataframe(df_exibicao, use_container_width=True, hide_index=True)
+                st.dataframe(
+                    df_exibicao, 
+                    use_container_width=True, 
+                    hide_index=True,
+                    column_config={
+                        "Descrição_Detalhada": st.column_config.TextColumn(
+                            "Descrição_Detalhada",
+                            width="large",
+                            disabled=True
+                        )
+                    }
+                )
 
                 st.divider()
                 st.subheader("🗑️ Gerenciar Exclusões")
