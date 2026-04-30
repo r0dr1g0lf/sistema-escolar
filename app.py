b2fae26fe8935bf7cae9e6b8bc0870d07b9a5019
r0dr1g0lf <rpdrigolf@gmail.com>
2026-04-30 11:27:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import gspread
 from google.oauth2.service_account import Credentials
 import time
+import io
 
 SHEET_ID = "153ohv6YsmfOZHjoLpb8He2VM2P-DYTVGh9zDVNRBdS0"
 
@@ -305,6 +306,17 @@ else:
                 }
                 
                 st.dataframe(df_exibicao_viz, use_container_width=True, hide_index=True, column_config=column_config)
+
+                # --- BOTÃO DE DOWNLOAD CONFIGURADO ---
+                csv_buffer = io.StringIO()
+                df_exibicao_viz.to_csv(csv_buffer, sep=';', index=False, encoding='utf-8-sig')
+                st.download_button(
+                    label="📥 SALVAR RELATÓRIO (.TXT)",
+                    data=csv_buffer.getvalue(),
+                    file_name=f"Relatorio_{datetime.now().strftime('%d_%m_%Y')}.txt",
+                    mime="text/plain",
+                )
+                # -------------------------------------
 
                 st.divider()
                 st.subheader("📝 Editar ou 🗑️ Excluir Registros")
