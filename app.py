48ca5e49d0200cc49d09a94f9b963bf813a3bb42
r0dr1g0lf <rpdrigolf@gmail.com>
2026-06-20 15:40:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -636,7 +636,7 @@ else:
             except Exception as e:
                 st.error(f"Erro ao carregar registros: {e}")
 
-    elif pagina_atual == "Ocorrências":
+    elif pagina_atual == "Ocorrencias":
         st.title("🚨 Registro de Ocorrências")
         tab_oc1, tab_oc2 = st.tabs(["Nova Ocorrência", "Visualizar Ocorrências"])
         
